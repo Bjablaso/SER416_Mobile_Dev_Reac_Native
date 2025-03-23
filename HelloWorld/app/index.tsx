8a37d19fe00f8e913647a740d45529d5e6620a1b
Brandon Jablasone <brandonjablasone@gmail.com>
2025-03-23 12:55:05 +0300

    Add multiple styling to text

--- HelloWorld/app/index.tsx
+++ HelloWorld/app/index.tsx
@@ -1,13 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+
 const index = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.Text}> Hello World </Text>
+      <Text style={styles.Text_Part1}> Hello, 
+        <Text style={styles.Text_Part2}>SER423 My Name is Brandon Jablasone  </Text>
+      </Text>
+    
     </View>
   )
 }
+
 
 export default index
 
@@ -17,10 +22,18 @@ export default index
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
   },
-    Text: {
+    Text_Part1: {
     fontSize: 24,
     fontWeight: 'bold',
     color: 'Black',
+    },
+    Text_Part2:{
+      fontSize: 24,
+      fontWeight: 'light',
+      color: 'Black',
     }
  })
